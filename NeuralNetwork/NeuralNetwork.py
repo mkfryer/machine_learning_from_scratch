25d5ecd0e8432f48711a552b7064de6d03ff83fd
Kohler <kohler.fryer@byu.edu>
2019-05-29 16:45:43 -0600

    finished project

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -12,8 +12,6 @@ def sigmoid_derivative(x):
 def matrix_mult(A, b):
         if type(b) != np.ndarray or type(A) != np.ndarray:
             return A * b
-        # elif type(b) == np.ndarray and b.shape == (1, 1) :
-        #     return A * b[0][0]
         else:
             return A @ b
 
@@ -28,56 +26,23 @@ class NeuralNetwork:
         self.feat_len = feat_len
         self.LR = LR
         self.input_len = input_len
-        self.weights_prev = []
         self.weights = []
         self.biases = []
-        self.biases_prev = []
         for i in range(1, len(hidden_layer_dims)):
             m = hidden_layer_dims[i]
             n = hidden_layer_dims[i-1]
             self.biases.append(
                 np.random.normal(mu, sigma, size = (m, 1)).astype(np.float64)
             )
-            self.biases_prev.append(
-                np.zeros((m, 1))
-            )
             self.weights.append( 
                 np.random.normal(mu, sigma, size = (m, n)).astype(np.float64)
             )
-            self.weights_prev.append(
-                np.zeros((m, n))
-            )
-        # self.biases.append(np.random.normal(mu, sigma, size = (self.feat_len, 1)).astype(np.float64))
-        # self.biases_prev.append(np.zeros((self.feat_len, 1)))
-        # self.weights.append(np.random.normal(mu, sigma, size = (self.feat_len, hidden_layer_dims[-1])).astype(np.float64))
-        # self.weights_prev.append(np.zeros((self.feat_len, hidden_layer_dims[-1])))
-
-        # self.W1_prev = np.zeros((hidden_layer_size, input_len))
-        # self.W2_prev = np.zeros((self.feat_len, hidden_layer_size))
-        # self.B2_prev = np.zeros((self.feat_len, 1))
-        # self.B1_prev = np.zeros((hidden_layer_size, 1))
-        # self.W1 = np.random.normal(mu, sigma, size = (hidden_layer_size, input_len)).astype(np.float64)
-        # self.W2 = np.random.normal(mu, sigma, size = (self.feat_len, hidden_layer_size)).astype(np.float64)
-        # self.B1 = np.random.normal(mu, sigma, size = (hidden_layer_size, 1)).astype(np.float64)
-        # self.B2 = np.random.normal(mu, sigma, size = (self.feat_len, 1)).astype(np.float64)
-
-    # def present_weights(self ):
-    #     print("W2", self.W2, "\n")
-    #     print("W1", self.W1, "\n")
-    #     print("B2", self.B2, "\n")
-    #     print("B1", self.B1, "\n")
     
     def predict(self, x):
         _, A = self.forwardpass(x)
-        # print(y_hat.flatten())
-        # print(np.argmax(y_hat))
-        # print("guess:", y_hat)
-        # return np.argmax(y_hat)
         return A[-1]
 
     def train_singleton(self, x, y):
-        # Z2, Z3, A2, A3 = self.forwardpass(x)
-        # self.backprop(Z2, Z3, A2, A3, x, y)
         Z, A = self.forwardpass(x)
         self.backprop(Z, A, x, y)
 
@@ -105,13 +70,14 @@ class NeuralNetwork:
         min_mse = np.inf
         all_mse_te = []
         all_mse_va = []
+        all_mse_tr = []
         all_acc_va = []
         
         while True:
             for i in range(w):
                 train_set.shuffle()
-                validation_set.shuffle()
-                train_set.shuffle()
+                # validation_set.shuffle()
+                test_set.shuffle()
 
                 #train
                 for j in range(m1):
@@ -123,9 +89,11 @@ class NeuralNetwork:
 
                 acc_va, mse_va = self.get_errors(validation_set)
                 acc_te, mse_te = self.get_errors(test_set)
+                acc_tr, mse_tr = self.get_errors(train_set)
 
                 all_acc_va.append(acc_va)
                 all_mse_va.append(mse_va)
+                all_mse_tr.append(mse_tr)
                 all_mse_te.append(mse_te)
 
             new_min_mse = min(all_mse_va[-w:])
@@ -135,7 +103,7 @@ class NeuralNetwork:
             else:
                 min_mse = new_min_mse
 
-        return all_acc_va, all_mse_va, all_mse_te
+        return all_acc_va, all_mse_va, all_mse_te, all_mse_tr
                 
 
     def forwardpass(self, x):
@@ -147,24 +115,14 @@ class NeuralNetwork:
             Wi_hat = np.hstack((self.weights[i], self.biases[i]))
             Z.append(Wi_hat @ Ai_hat)
             A.append(sigmoid(Z[i]))
-        # x_hat = np.vstack((x, [[1]]))
-        # W1_hat = np.hstack((self.W1, self.B1))
-        # Z2 = W1_hat @ x_hat
-        # A2 = sigmoid(Z2)
-        # A2_hat = np.vstack((A2, [[1]]))
-        # W2_hat = np.hstack((self.W2, self.B2))
-        # Z3 = W2_hat @ A2_hat
-        # A3 = sigmoid(Z3)
-        # return Z2, Z3, A2, A3
         return Z, A
 
-    # def backprop(self, Z2, Z3, A2, A3, x, y):
     def backprop(self, Z, A, x, y):
 
         Delta = -(y - A[-1]) * sigmoid_derivative(Z[-1])
         for i in range(len(self.weights))[::-1]:
-            D_Wi = self.LR * (matrix_mult(Delta, A[i].T)) + self.momentum * self.weights_prev[i]
-            D_Bi = self.LR * Delta + self.momentum * self.biases_prev[i]
+            D_Wi = self.LR * (matrix_mult(Delta, A[i].T)) + self.momentum * self.weights[i]
+            D_Bi = self.LR * Delta + (self.momentum * self.biases[i])
             self.weights[i] -= D_Wi
             self.biases[i] -= D_Bi
             
@@ -172,39 +130,14 @@ class NeuralNetwork:
                 Delta = matrix_mult(self.weights[i].T, Delta) * sigmoid_derivative(Z[i-1])
 
 
-        self.biases_prev = self.biases.copy()
-        self.weights_prev = self.weights.copy()
-        # Delta3 = -(y - A3) * sigmoid_derivative(Z3)
-        # Delta2 = (self.W2.T @ Delta3) * sigmoid_derivative(Z2)
-
-        # D_W2 = self.LR * (Delta3 @ A2.T) + self.momentum * self.W2_prev
-        # D_W1 = self.LR * (Delta2 @ x.T) + self.momentum * self.W1_prev
-        # D_B2 = self.LR * (Delta3) + self.momentum * self.B2_prev
-        # D_B1 = self.LR * (Delta2) + self.momentum * self.B1_prev
-        
-        # self.W2 -= D_W2 
-        # self.W1 -= D_W1
-        # self.B2 -= D_B2
-        # self.B1 -= D_B1
-
-
 def test_basics():
     nn = NeuralNetwork(2, [2], 1, LR = 1)
-    # print("actual", nn.weights[0].shape, nn.weights[1].shape)
     nn.weights = [np.ones((2, 2)).astype(np.float64), np.ones((1, 2)).astype(np.float64)]
-    # print("goal", nn.weights[0].shape, nn.weights[1].shape)
     nn.weights_prev = [np.zeros((2, 2)).astype(np.float64), np.zeros((1, 2)).astype(np.float64)]
-    # print(nn.biases[0].shape, nn.biases[1].shape)
     nn.biases = [np.ones((2, 1)).astype(np.float64), np.array([[1]]).astype(np.float64)]
-    # print(nn.biases[0].shape, nn.biases[1].shape)
-    # print(type(nn.biases_prev))
-    # print(type([np.zeros((2, 1)), np.zeros([[1]])]))
     nn.biases_prev = [np.zeros((2, 1)).astype(np.float64), np.zeros((1, 1)).astype(np.float64)]
     nn.feat_len = 1
-    #nn.W1 = np.ones((2, 2))
-    # nn.W2 = np.ones((1, 2))
-    # nn.B1 = np.ones((2, 1))
-    # nn.B2 = np.array([[1.0]])
+
     x = np.zeros((2, 1)).astype(np.float64)
     y = np.array([1]).astype(np.float64)
     nn.train_singleton(x, y)
--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -15,7 +15,7 @@ def prob2():
     validation_set = arff.create_subset_arff(row_idx=slice(v, n, 1))
 
     nn = NeuralNetwork(4, [9], 3, LR=.1)
-    all_acc_va, all_mse_va, all_mse_te = nn.train_set(train_set, test_set, validation_set)
+    all_acc_va, all_mse_va, all_mse_te, all_mse_tr = nn.train_set(train_set, test_set, validation_set)
  
     d = [x for x in range(len(all_acc_va))]
     plt.plot(d, all_mse_te, label="test MSE")
@@ -38,20 +38,126 @@ def prob3():
     train_set = arff.create_subset_arff(row_idx=slice(0, t, 1), col_idx = imp_atts)
     test_set = arff.create_subset_arff(row_idx=slice(t, v, 1), col_idx = imp_atts)
     validation_set = arff.create_subset_arff(row_idx=slice(v, n, 1), col_idx = imp_atts)
-    nn = NeuralNetwork(8, [16, 14], 11, LR=.01, momentum=0)
-    all_acc_va, all_mse_va, all_mse_te = nn.train_set(train_set, test_set, validation_set, w = 5)
 
+    best_mse_te = []
+    best_mse_tr = []
+    best_mse_va = []
+    epochs = []
+
+    LRS = [.01, .1, .5, .8, 1.5]
+    for LR in LRS:
+        # print(LR)
+        nn = NeuralNetwork(8, [16], 11, LR=LR, momentum=0)
+        all_acc_va, all_mse_va, all_mse_te, all_mse_tr = nn.train_set(train_set, test_set, validation_set, w = 5)
+        best_mse_te.append(min(all_mse_te))
+        best_mse_tr.append(min(all_mse_tr))
+        best_mse_va.append(min(all_mse_va))
+        epochs.append(len(all_mse_va))
+
+    plt.plot(LRS, best_mse_te, label="MSE Te")
+    plt.plot(LRS, best_mse_tr, label="MSE Tr")
+    plt.plot(LRS, best_mse_va, label="MSE V.A")
+    plt.title("Vowel MSE vs Learning Rate")
+    plt.xlabel("Learning Rate")
+    plt.ylabel("MSE")
+    plt.legend()
+    plt.show()
+
+    plt.plot(LRS, epochs)
+    plt.title("Vowel Epochs vs Learning Rate")
+    plt.xlabel("Learning Rate")
+    plt.ylabel("Epochs")
+    plt.legend()
+    plt.show()
+
+def prob4():
+    arff = Arff(sys.argv[2])
+    imp_atts = [1, 3, 4, 5, 7, 9, 11, 12, 13]
+    arff.shuffle()
+    n = len(arff.get_labels().data)
+    t = int(n * .55)
+    v = n - int(n * .20)
+    train_set = arff.create_subset_arff(row_idx=slice(0, t, 1), col_idx = imp_atts)
+    test_set = arff.create_subset_arff(row_idx=slice(t, v, 1), col_idx = imp_atts)
+    validation_set = arff.create_subset_arff(row_idx=slice(v, n, 1), col_idx = imp_atts)
+
+    best_mse_te = []
+    best_mse_tr = []
+    best_mse_va = []
+    hidden_nodes = [1, 3, 6, 10, 13, 15, 16, 18, 20, 22, 25, 30, 40]
+
+    for nodes in hidden_nodes:
+        # print(nodes)
+        nn = NeuralNetwork(8, [nodes], 11, LR = .1, momentum=0)
+        all_acc_va, all_mse_va, all_mse_te, all_mse_tr = nn.train_set(train_set, test_set, validation_set, w = 5)
+        
+        best_mse_te.append(min(all_mse_te))
+        best_mse_tr.append(min(all_mse_tr))
+        best_mse_va.append(min(all_mse_va))
+
+    plt.plot(hidden_nodes, best_mse_te, label="MSE Te")
+    plt.plot(hidden_nodes, best_mse_tr, label="MSE Tr")
+    plt.plot(hidden_nodes, best_mse_va, label="MSE V.A")
+    plt.title("Vowel MSE vs Hidden Nodes")
+    plt.xlabel("Hidden Nodes")
+    plt.ylabel("MSE")
+    plt.legend()
+    plt.show()
+
+def prob5():
+    arff = Arff(sys.argv[2])
+    imp_atts = [1, 3, 4, 5, 7, 9, 11, 12, 13]
+    arff.shuffle()
+    n = len(arff.get_labels().data)
+    t = int(n * .55)
+    v = n - int(n * .20)
+    train_set = arff.create_subset_arff(row_idx=slice(0, t, 1), col_idx = imp_atts)
+    test_set = arff.create_subset_arff(row_idx=slice(t, v, 1), col_idx = imp_atts)
+    validation_set = arff.create_subset_arff(row_idx=slice(v, n, 1), col_idx = imp_atts)
+
+    epochs = []
+    momentums = np.linspace(0, 1.5, 20)
+    # momentums = [.5, 1]
+
+    for momentum in momentums:
+        print(momentum)
+        nn = NeuralNetwork(8, [30], 11, LR = .1, momentum=momentum)
+        all_acc_va, all_mse_va, all_mse_te, all_mse_tr = nn.train_set(train_set, test_set, validation_set, w = 5)
+        epochs.append(len(all_acc_va))
+
+    plt.plot(momentums, epochs)
+    plt.title("Vowel Momentum vs Epoch Convergence")
+    plt.xlabel("Momentum")
+    plt.ylabel("Epochs til Conv.")
+    plt.show()
+
+def prob6():
+    arff = Arff(sys.argv[1])
+    arff.shuffle()
+    n = len(arff.get_labels().data)
+    t = int(n * .55)
+    v = n - int(n * .20)
+    train_set = arff.create_subset_arff(row_idx=slice(0, t, 1))
+    test_set = arff.create_subset_arff(row_idx=slice(t, v, 1))
+    validation_set = arff.create_subset_arff(row_idx=slice(v, n, 1))
+
+    nn = NeuralNetwork(4, [9, 8, 8, 7, 9, 8], 3, LR=.01)
+    all_acc_va, all_mse_va, all_mse_te, all_mse_tr = nn.train_set(train_set, test_set, validation_set)
+ 
     d = [x for x in range(len(all_acc_va))]
     plt.plot(d, all_mse_te, label="test MSE")
     plt.plot(d, all_mse_va, label="Val. MSE")
     plt.plot(d, all_acc_va, label="Val. Accuracy")
-    plt.title("Vowel Dataset")
+    plt.title("Iris Dataset W/ 6 hidden layers")
     plt.xlabel("Epochs")
     plt.ylabel("%")
     plt.legend()
     plt.show()
 
-
 if __name__ == "__main__":
     test_basics()
+    prob2()
     prob3()
+    prob4()
+    prob5()
+    prob6()
