b181268f2919c94c898d1a0a02bb140373b2f4dc
Kohler <kohler.fryer@byu.edu>
2019-06-01 21:12:50 -0600

    cleaned things up a bit and added more abstraction

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -2,50 +2,50 @@
 Decision Tree
 """
 import numpy as np
+import pandas as pd
 
 class Branch:
 
-    def __init__(self, attr_idx, cat_idx):
-        self.attr_idx = attr_idx
-        self.cat_idx = cat_idx
+    def __init__(self, category):
+        self.category = category
         self.c_node = None
 
     def add_connection(self, node):
         self.c_node = node
 
-    # def show(self):
-    #     if self.c_node != None:
-    #         print(self.c_node.get_class())
-    #         for branch in self.c_node.branches:
-    #             branch.show()
-
 class Node:
-
-    def __init__(self, data, attr_idx):
-        # self.children = children
+    def __init__(self, data, attribute):
         self.data = data
         self.branches = []
-        self.attr_idx = attr_idx
+        self.attribute = attribute
 
     def connect_branch(self, branch):
         self.branches.append(branch)
 
-    def get_class(self):
-        cols = ["outlook", "temperature", "humidity" ,"wind", "playtennis"]
-        print(cols[self.attr_idx])
+    def get_class(self, s = ""):
+        s = s + " node:" + self.attribute
         for branch in self.branches:
-            branch.c_node.get_class()
+            if type(branch.c_node) == Node:
+                branch.c_node.get_class(s + " branch:" + branch.category)
+            elif type(branch.c_node) == LeafNode:
+                print(s + " branch:" + branch.category +  " label:" + branch.c_node.label)
+                
         print("exausted branches")
 
+class LeafNode(Node):
+    def __init__(self, data, label):
+        super().__init__(data, None)
+        self.label = label
 
 
 class DecisionTree:
-    def __init__(self, data):
+    def __init__(self, data, attributes):
         """
         """
         self.data = data
         self.m, self.n = data.shape
         self.root = None
+        self.attributes = attributes
     
     def calc_entropy(self, targ_col_data):
         uniq = np.unique(targ_col_data)
@@ -60,10 +60,10 @@ class DecisionTree:
     def calc_gain(self, data, attr_index):
         """ """
         m, n = data.shape
-        entr_global = self.calc_entropy(self.data[:, -1])
+        entr_global = self.calc_entropy(data[:, -1])
         sub_entr = []
         categories = np.unique(data[:, attr_index])
-       
+
         for cat in categories:
             cat_mask = np.where(data == cat)[0]
             sub_entr.append((len(cat_mask)/m) * self.calc_entropy(data[:, -1][cat_mask]))
@@ -78,7 +78,6 @@ class DecisionTree:
         for attr_idx in active_attr_indxs:
             gains[attr_idx] = self.calc_gain(data, attr_idx)
         max_gain_index = np.argmax(gains)
-
         return max_gain_index
 
     def split_data(self, data, i):
@@ -86,81 +85,96 @@ class DecisionTree:
         i - index of column(attr) to split on
         """
         attributes = np.unique(data[:, i])
-        splits = []
+        splits = {}
         for attr in attributes:
             idx_mask, = np.where(data[:, i] == attr)
-            splits.append(data[idx_mask, :])
+            splits[attr] = data[idx_mask, :]
         return splits
         
     def start_learn(self, data):
         #attributes to split on. Can split if attr_indx = 1, 0 means you cant use it
         attrs_idx_states = np.ones(self.n)
         #dont split on category
-        attrs_idx_states[-1] = 0 
+        attrs_idx_states[-1] = 0
         #index corresponding to best gain
         bst_attr_idx = self.find_max_gain(data, attrs_idx_states)
-        self.root = Node(data, bst_attr_idx)
+        self.root = Node(data, self.attributes[bst_attr_idx])
 
         data_attr_splits = self.split_data(data, bst_attr_idx)
         # avail_attrs_idxs = np.delete(avail_attrs_idxs, [bst_attr_idx], None)
         #set state used attr_idx as used
         attrs_idx_states[bst_attr_idx] = 0
-        for i, data_split in enumerate(data_attr_splits):
-            branch = Branch(bst_attr_idx, i)
+        for key in data_attr_splits.keys():
+            data_split = data_attr_splits[key]
+            branch = Branch(key)
             self.root.connect_branch(branch)
             self.learn(data_split, branch, attrs_idx_states.copy())
 
     def learn(self, data, branch, attrs_idx_states):
-        bst_attr_idx = self.find_max_gain(data, attrs_idx_states)
-        child = Node(data, bst_attr_idx)
-        branch.add_connection(child)
+        #base case - stop learning when data is pure
+        if len(np.unique(data[:, -1])) == 1:
+            #create leaf node and get label
+            child = LeafNode(data, data[0, -1])
+            branch.add_connection(child)
+            return
 
+        bst_attr_idx = self.find_max_gain(data, attrs_idx_states)
         #set state used attr_idx as used
         attrs_idx_states[bst_attr_idx] = 0
-        data_attr_splits = self.split_data(data, bst_attr_idx)
+        child = Node(data, self.attributes[bst_attr_idx])
+        branch.add_connection(child)
 
-        for i, data_split in enumerate(data_attr_splits):
-            #base case - learn only when data_split is not pure
-            if len(np.unique(data[:, -1])) > 1:
-                branch = Branch(bst_attr_idx, i)
-                child.connect_branch(branch)
-                self.learn(data_split, branch, attrs_idx_states.copy())
+        data_attr_splits = self.split_data(data, bst_attr_idx)
+        for key in data_attr_splits.keys():
+            data_split = data_attr_splits[key]
+            branch = Branch(key)
+            child.connect_branch(branch)
+            self.learn(data_split, branch, attrs_idx_states.copy())
 
     def show_tree(self):
         """ """
         self.root.get_class()
 
 if __name__ == "__main__":
-    # print(
-    #     # (1/3) * np.log2(1/3) + (2/3) * np.log2(2/3)
-    #     # .919 + np.log2(1/3)
-    #     # (2/9) * np.log2(1/2) - .113
-    #     (2/9) * np.log(1)
-    # )
 
+    f =  np.array([
+            ["sunny", "sunny", "overcast", "rainy", "rainy", "rainy", "overcast", "sunny", "sunny", "rainy", "sunny", "overcast", "overcast", "rainy"],
+            ["hot", "hot", "hot", "mild", "cool", "cool", "cool", "mild", "cool", "mild", "mild", "mild", "hot", "mild"],
+            ["high", "high", "high", "high", "normal", "normal", "normal", "high", "normal", "normal", "normal", "high", "normal", "high"],
+            ["weak", "strong", "weak", "weak", "weak", "strong", "strong", "weak", "weak", "weak", "strong", "strong", "weak", "strong"],
+            ["no", "no", "yes", "yes", "yes", "no", "yes", "no", "yes", "yes", "yes" , "yes", "yes", "no"],
+        ]).T
+    attributes = ["outlook", "temperature", "humidity" ,"wind", "playtennis", "no", "yes"]
+    T = DecisionTree(f, attributes)
+    T.start_learn(f)
+    T.show_tree()
 
-    a = ["sunny", "sunny", "overcast", "rainy", "rainy", "rainy", "overcast", "sunny", "sunny", "rainy", "sunny", "overcast", "overcast", "rainy"]
-    b = ["hot", "hot", "hot", "mild", "cool", "cool", "cool", "mild", "cool", "mild", "mild", "mild", "hot", "mild"]
-    c = ["high", "high", "high", "high", "normal", "normal", "normal", "high", "normal", "normal", "normal", "high", "normal", "high"]
-    d = ["weak", "strong", "weak", "weak", "weak", "strong", "strong", "weak", "weak", "weak", "strong", "strong", "weak", "strong"]
-    e = ["no", "no", "yes", "yes", "yes", "no", "yes", "no", "yes", "yes", "yes" , "yes", "yes", "no"]
+    # f = np.chararray([a, b, c, d, e])
 
-    f = [a, b, c, d, e]
-    # keys = set()
-    # for x in f:
-    #     for y in x:
-    #         keys.add(y)
+    # print(f)
+
+    # category_encoding = np.zeros((len(f[0]), len(f)))
+    # for i, x in enumerate(f):
+    #     category_encoding[:, i] = pd.Series(x, dtype="category").cat.codes.values
+    # attribute_encoding = pd.Series(attributes, dtype="category").cat.codes.values
+
+    # data = np.zeros((len(f[0]), len(f)))
+    # for i in len(f):
+    #     data[:, i] = category_encoding[i]
+    # print(category_encoding)
+    # keys = ["sunny", "rainy", "overcast", "hot", "mild", "cool", "high", "normal", "weak", "strong", "no", "yes"]
+    # d_k = {key:x for x,key in enumerate(keys)}
     # print(keys)
+    # cols = ["outlook", "temperature", "humidity" ,"wind", "playtennis"]
+    # k_c = {key:x for x,key in enumerate(cols)}
+    # data = np.zeros((14, 5))
+    
+    # terms = ["sunny", "rainy", "overcast", "hot", "mild", "cool", "high", "normal", "weak", "strong", "no", "yes", "outlook", "temperature", "humidity" ,"wind", "playtennis"]
+    # encoding = {key:x for x,key in enumerate(cols)}
 
-    keys = ["sunny", "rainy", "overcast", "hot", "mild", "cool", "high", "normal", "weak", "strong", "no", "yes"]
-    d_k = {key:x for x,key in enumerate(keys)}
-    cols = ["outlook", "temperature", "humidity" ,"wind", "playtennis"]
-    k_c = {key:x for x,key in enumerate(cols)}
-    data = np.zeros((14, 5))
-
-    for j, x in enumerate(f):
-        for i, y in enumerate(x):
-            data[i, j] = d_k[y]
+    # for j, x in enumerate(f):
+    #     for i, y in enumerate(x):
+    #         data[i, j] = category_encoding[j, i]
 
     # print(data)
     # print(data)
@@ -177,10 +191,7 @@ if __name__ == "__main__":
     #     [0, 2, 5, 8],
     #     ])
     # print(a)
-    T = DecisionTree(data)
     # print(T.calc_gain(a, 0))
     # print(T.calc_gain(a, 1))
     # print(T.calc_gain(a, 2))
     # print(T.find_max_gain(a, np.array([1, 1, 0])))
-    T.start_learn(data)
-    T.show_tree()
