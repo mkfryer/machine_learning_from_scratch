af42c8885d458d75daea06e60c1bbc54c58bc186
Kohler <kohler.fryer@byu.edu>
2019-06-13 17:46:42 -0600

    fixed distance bug

--- Clustering/KMeansClustering.py
+++ Clustering/KMeansClustering.py
@@ -1,7 +1,7 @@
 import numpy as np
 from toolkit.arff import Arff
 from scipy.spatial.distance import cdist
-
+from matplotlib import pyplot as plt
 class KMC:
 
     def __init__(self, k, train_data, test_data, attr_types):
@@ -11,22 +11,27 @@ class KMC:
         self.attr_types = attr_types
         self.m, self.n = train_data.shape
         self.centroids = train_data[:k, :].copy()
-        # print("initial centroids", self.centroids, "\n")
+        print("initial centroids \n", self.centroids, "\n")
 
     def get_sqrd_dist(self, x, y):
         sqrd_dist = 0
         n = len(x)
+        # print("x", x)
+        # print("y", y)
         for i in range(n):
-            if self.attr_types[i] == 'real':
-                if np.isnan(x[i]) or np.isnan(y[i]):
-                    sqrd_dist += 1
-                else:
-                    sqrd_dist += (x[i] - y[i])**2
-            else:
-                if np.isnan(x[i]) or np.isnan(y[i]):
-                    sqrd_dist += 1
-                elif x[i] != y[i]:
-                    sqrd_dist += 1
+            if np.isnan(x[i]) or np.isnan(y[i]):
+                # print("nan", x[i], y[i])
+                sqrd_dist += 1
+            elif self.attr_types[i] == 'real':
+                # print("real", x[i], y[i])
+                sqrd_dist += (x[i] - y[i])**2
+            elif x[i] != y[i]:
+                # print("cat not equal", x[i], y[i])
+                sqrd_dist += 1
+            # else:
+                # print('Cat equal', x[i], y[i])
+
+            # print("dist: ", sqrd_dist)
         return sqrd_dist
 
     def calculate_centroid(self, cluster):
@@ -34,7 +39,7 @@ class KMC:
         cluster = np.array(cluster)
         m, n = cluster.shape
         for i in range(n):
-            if self.attr_types[i] == 'real':
+            if self.attr_types[i] == "real":
                 x = cluster[:, i]
                 x = x[~np.isnan(x)]
                 # print("x:", x)
@@ -57,7 +62,7 @@ class KMC:
     # def populate_clusters():
 
     def train(self, get_distance):
-
+        
         #populate clusters and calculate new centroids
         clusters = [[] for x in range(self.k)]
         for i in range(self.m):
@@ -71,6 +76,7 @@ class KMC:
             l = np.argmin(distances)
             # print("chosen centroid: ", self.centroids[l])
             clusters[l].append(x)
+            # print(i, "=", l)
         
         # print("clusters: \n", clusters, "\n")
         for i in range(self.k):
@@ -100,12 +106,41 @@ def test_1():
     assert(np.allclose(centroids_t, kmc.centroids))
     print("test 1: pass")
 
+def show_table(m, title = ""):
+    fig, axs = plt.subplots(1,1)
+    axs.axis('tight')
+    axs.axis('off')
+    axs.table(
+            cellText=m,
+            loc='center'
+        )
+    axs.set_title(title)
+    plt.show()
+
+
 def test_cases():
     # test_1()
 
 
+# @attribute 'duration' real
+# @attribute 'wage-increase-first-year' real
+# @attribute 'wage-increase-second-year' real
+# @attribute 'wage-increase-third-year' real
+# @attribute 'cost-of-living-adjustment' {'none','tcf','tc'}
+# @attribute 'working-hours' real
+# @attribute 'pension' {'none','ret_allw','empl_contr'}
+# @attribute 'standby-pay' real
+# @attribute 'shift-differential' real
+# @attribute 'education-allowance' {'yes','no'}
+# @attribute 'statutory-holidays' real
+# @attribute 'vacation' {'below_average','average','generous'}
+# @attribute 'longterm-disability-assistance' {'yes','no'}
+# @attribute 'contribution-to-dental-plan' {'none','half','full'}
+# @attribute 'bereavement-assistance' {'yes','no'}
+# @attribute 'contribution-to-health-plan' {'none','half','full'}
+# @attribute 'class' {'bad','good'}
+
     attr_types = [
-        "real",
         "real",
         "real",
         "real",
@@ -129,8 +164,22 @@ def test_cases():
     features = arff.get_features().data
     labels = arff.get_labels().data
     # attributes = arff.get_attr_names()
-    data = np.hstack((features, labels))
+    data = np.hstack((features, labels))[:, 1:]
     kmc = KMC(k, data, data, attr_types)
     kmc.train(kmc.get_sqrd_dist)
+    print("first centroid \n", kmc.centroids)
+    kmc.train(kmc.get_sqrd_dist)
+    print("second centroid \n", kmc.centroids)
+
+    # print(kmc.get_sqrd_dist(data[3, :], data[8, :]))
+    # print(kmc.get_sqrd_dist(data[1, :], data[8, :]))
+
+    # show_table(
+    #     [data[3, :], data[8, :], data[1, :]]
+    # )
 
 test_cases()
+
+
+
+
