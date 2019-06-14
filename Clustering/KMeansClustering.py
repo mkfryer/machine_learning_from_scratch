73372ae3ef088794509fe060ab05dfd4528f238b
Kohler <kohler.fryer@byu.edu>
2019-06-14 08:09:33 -0600

    ..

--- Clustering/KMeansClustering.py
+++ Clustering/KMeansClustering.py
@@ -11,27 +11,17 @@ class KMC:
         self.attr_types = attr_types
         self.m, self.n = train_data.shape
         self.centroids = train_data[:k, :-1].copy()
-        print("initial centroids \n", self.centroids, "\n")
 
     def get_sqrd_dist(self, x, y):
         sqrd_dist = 0
         n = len(x)
-        # print("x", x)
-        # print("y", y)
         for i in range(n):
             if np.isnan(x[i]) or np.isnan(y[i]):
-                # print("nan", x[i], y[i])
                 sqrd_dist += 1
             elif self.attr_types[i] == 'real':
-                # print("real", x[i], y[i])
                 sqrd_dist += (x[i] - y[i])**2
             elif x[i] != y[i]:
-                # print("cat not equal", x[i], y[i])
                 sqrd_dist += 1
-            # else:
-                # print('Cat equal', x[i], y[i])
-
-            # print("dist: ", sqrd_dist)
         return sqrd_dist
 
     def calculate_centroid(self, cluster):
@@ -42,50 +32,75 @@ class KMC:
             if self.attr_types[i] == "real":
                 x = cluster[:, i]
                 x = x[~np.isnan(x)]
-                # print("x:", x)
                 if len(x) > 0:
-                    # print("x", x)
                     centroid[i] = np.mean(x)
-                    
                 else:
                     centroid[i] = np.nan
             else:
                 x = cluster[:, i]
-                # print("x", x)
                 x = x[~np.isnan(x)].astype(int)
                 if len(x) > 0:
-                    # print("argmax", np.bincount(x).argmax())
                     centroid[i] = np.bincount(x).argmax()
                 else:
                     centroid[i] = np.nan
-            # print("centroid", centroid[i])
         return centroid
     
-    # def populate_clusters():
+    def predict(self, x):
+        distances = np.zeros(self.k)
+        for j in range(self.k):
+            distances[j] = self.get_sqrd_dist(x, self.centroids[j])
+        return np.argmin(distances)
 
-    def train(self, get_distance):
-        
-        #populate clusters and calculate new centroids
-        clusters = [[] for x in range(self.k)]
-        for i in range(self.m):
-            x = self.train_data[i, :-1].copy()
-            distances = np.zeros(self.k)
-            # print("vector:", x)
-            for j in range(self.k):
-                # print("cluster:", self.centroids[j])
-                distances[j] = get_distance(x, self.centroids[j])
-            # print("d", distances, "\n")
-            l = np.argmin(distances)
-            # print("chosen centroid: ", self.centroids[l])
-            clusters[l].append(x)
-            # print(i, "=", l)
-        
-        # print("clusters: \n", clusters, "\n")
+    def get_accuracy(self):
+        m, n = self.test_data.shape
+        err = 0
+        for i in range(m):
+            y = self.predict(self.test_data[i, :-1])
+            y_hat = self.test_data[i, -1]
+            err += self.get_sqrd_dist([y], [y_hat])
+        return err
+
+    def report(self):
+        print("Total Clusters:", self.k, "\n")
+        err = 0
         for i in range(self.k):
-            print("c", i, "\n")
-            self.centroids[i] = self.calculate_centroid(clusters[i])
-        
-        # print("centroids:", self.centroids)
+            centroid = self.centroids[i]
+            cluster = self.clusters[i]
+            n = len(cluster)
+            c_err = 0
+            for j in range(n):
+                c_err += self.get_sqrd_dist(cluster[j], centroid)
+            err += c_err
+            print("Cluster:", i)
+            print("Centroid:", "\n", centroid)
+            print("Instances in centroid:", n)
+            print("Centroid SSE:", c_err)
+        print("Total SSE:", err, "\n \n \n")
+            
+    def train(self, tol = .0001):
+        last_err, cur_err = (np.inf, 0)
+        clusters_err = []
+
+        while (cur_err - last_err)**2 > tol:
+            last_err = cur_err
+            cur_err = 0
+
+            #populate clusters
+            self.clusters = [[] for x in range(self.k)]
+            for i in range(self.m):
+                x = self.train_data[i, :-1].copy()
+                distances = np.zeros(self.k)
+                for j in range(self.k):
+                    distances[j] = self.get_sqrd_dist(x, self.centroids[j])
+                l = np.argmin(distances)
+                cur_err += distances[l]
+                self.clusters[l].append(x)
+
+            #calculate new centroids
+            for i in range(self.k):
+                self.centroids[i] = self.calculate_centroid(self.clusters[i])
+
+            self.report()
 
 
 def test_1():
@@ -124,25 +139,6 @@ def show_table(m, title = ""):
 def test_cases():
     # test_1()
 
-
-# @attribute 'duration' real
-# @attribute 'wage-increase-first-year' real
-# @attribute 'wage-increase-second-year' real
-# @attribute 'wage-increase-third-year' real
-# @attribute 'cost-of-living-adjustment' {'none','tcf','tc'}
-# @attribute 'working-hours' real
-# @attribute 'pension' {'none','ret_allw','empl_contr'}
-# @attribute 'standby-pay' real
-# @attribute 'shift-differential' real
-# @attribute 'education-allowance' {'yes','no'}
-# @attribute 'statutory-holidays' real
-# @attribute 'vacation' {'below_average','average','generous'}
-# @attribute 'longterm-disability-assistance' {'yes','no'}
-# @attribute 'contribution-to-dental-plan' {'none','half','full'}
-# @attribute 'bereavement-assistance' {'yes','no'}
-# @attribute 'contribution-to-health-plan' {'none','half','full'}
-# @attribute 'class' {'bad','good'}
-
     attr_types = [
         "real",
         "real",
@@ -169,21 +165,8 @@ def test_cases():
     # attributes = arff.get_attr_names()
     data = np.hstack((features, labels))[:, 1:]
     kmc = KMC(k, data, data, attr_types)
-    kmc.train(kmc.get_sqrd_dist)
-    # print("first centroid \n", kmc.centroids[:, -1])
-    kmc.train(kmc.get_sqrd_dist)
-    # print("second centroid \n", kmc.centroids[:, -1])
+    kmc.train(tol=0)
 
-    # x = np.array([4, 30 , 30, 4])
-    # print(x)
-    # print(np.bincount(x).argmax())
-
-    # print(kmc.get_sqrd_dist(data[3, :], data[8, :]))
-    # print(kmc.get_sqrd_dist(data[1, :], data[8, :]))
-
-    # show_table(
-    #     [data[3, :], data[8, :], data[1, :]]
-    # )
 
 test_cases()
 
